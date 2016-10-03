c44840a6c81c1ef9736eec27d812c7db113cb94c
gavila94 <gabriellaravila@gmail.com>
2016-10-02 22:37:08 -0400

    Add files via upload

---
+++ Lab9_New/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_New
+{
+   public class Circle
+    {
+        public double radius = 0;
+        public static int numberOfCircle = 0;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            numberOfCircle++;
+        }
+        public static int getObjectCount()
+        {
+
+            return numberOfCircle;
+        }
+
+        public double getArea()
+        {
+            double areaOfACircle = Math.PI * radius * radius;
+            return areaOfACircle;
+        }
+
+        public double getCircumference()
+        {
+            double circumferenceOfACircle = 2 * Math.PI * radius;
+            return circumferenceOfACircle;
+
+        }
+
+        private String formatNumber(double valueToFormat)
+        {
+            string formattedValue = Math.Round(valueToFormat, 2).ToString();
+
+            return formattedValue;
+        }
+
+        public string getFormattedCircumference()
+        {
+            double formattedCircumference = this.getCircumference();
+            String valueToBeDisplayed = this.formatNumber(formattedCircumference);
+
+            return valueToBeDisplayed;
+        }
+
+        public String getFormattedArea()
+        {
+            double formattedArea = this.getArea();
+            String valueToBeDisplayed = this.formatNumber(formattedArea);
+            return valueToBeDisplayed;
+        }
+
+    }
+}
---
+++ Lab9_New/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Circle Tester\n");
+
+            string repeat = "";
+            do
+            {
+                string inputdata = "";
+                double radiusOfCircle = 0;
+
+                
+                {
+                    Console.Write("Enter Radius:\n ");
+                    inputdata = Console.ReadLine();
+                   
+
+                } 
+
+
+                Circle circleRadiusCircumference = new Circle(radiusOfCircle);
+
+                Console.WriteLine($"Circumference: {circleRadiusCircumference.getFormattedCircumference()}");
+
+                Console.WriteLine($"Area: {circleRadiusCircumference.getFormattedArea()}");
+
+
+                Console.Write("Do you want to Continue?(y/n)\n");
+                repeat = Console.ReadLine().Trim().ToLower();
+            } while (repeat == "y");
+
+            Console.WriteLine($"Goodbye. You created {Circle.getObjectCount()} Circle Object(s).\n ");
+
+        }
+    }
+}
